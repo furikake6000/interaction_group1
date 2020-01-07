6a05bd1ad4025ef0aeacc7a733056b60637ee64a
furikake6000 <ewave63@keio.jp>
2020-01-07 12:24:53 +0900

    音

---
+++ Assets/Scripts/Rhythm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rhythm : MonoBehaviour
+{
+
+    [SerializeField] float interval = 1f;
+    [SerializeField] AudioSource pingpong;
+
+    float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= interval) {
+            elapsedTime -= interval;
+            pingpong.Play();
+            Debug.Log("sound!");
+        }
+    }
+}
