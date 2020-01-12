8c6d9922f0b47ef96a7018ce854d8d9fe5ebab7a
takasago <vipqiv00@gmail.com>
2020-01-12 10:35:31 +0900

    リザルト画面にスコア表示

--- Assets/Scripts/AccelInput.cs
+++ Assets/Scripts/AccelInput.cs
@@ -12,6 +12,22 @@ public class AccelInput : MonoBehaviour
     [SerializeField, Range(0.0f, 10.0f)]
     float threshold;
 
+    public static int perfectCounter = 0;
+    public static int goodCounter = 0;
+    public static int missCounter = 0;
+
+    public static int getPerfect() {
+        return perfectCounter;
+    }
+
+    public static int getGood() {
+        return goodCounter;
+    }
+
+    public static int getMiss() {
+        return missCounter;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/ResultManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    public GameObject scorePerfectObj = null;
+    public GameObject scoreGoodObj = null;
+    public GameObject scoreMissObj = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int scorePerfect = AccelInput.getPerfect();
+        int scoreGood = AccelInput.getGood();
+        int scoreMiss = AccelInput.getMiss();
+        Text scorePerfectText = scorePerfectObj.GetComponent<Text> ();
+        Text scoreGoodText = scoreGoodObj.GetComponent<Text> ();
+        Text scoreMissText = scoreMissObj.GetComponent<Text> ();
+        scorePerfectText.text = scorePerfect.ToString();
+        scoreGoodText.text = scoreGood.ToString();
+        scoreMissText.text = scoreMiss.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
